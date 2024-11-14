e15af2e6c293a87e54702ffb7cdad2dc27c67c8f
Paul-HenryP <104301931+Paul-HenryP@users.noreply.github.com>
2024-11-14 19:57:27 +0200

    Update estScript.js

--- estScript.js
+++ estScript.js
@@ -258,7 +258,7 @@ function checkRoom() {
 // Modified checkForNearbyEntities to accept a message parameter.
 function checkForNearbyEntities(existingMessage) {
     let messages = [];
-    if (isNearby(ghostPosition) && !layoutChanged) {
+    if (isNearby(ghostPosition) && !layoutChanged && !hasKey) {
         messages.push("Kummitus on läheduses. ");
     }
     if (isNearby(keyPosition) && !hasKey) {
